ff0cd1713b7e6772b59f988c3a06dde5a7dee358
aboobakkar <aboobakkar.mec@gmail.com>
2021-10-01 20:47:39 +0530

    basic profile details added

---
+++ src/components/profile.js
@@ -0,0 +1,111 @@
+import React, {Component} from 'react';
+import {TextField, Button, Container} from '@material-ui/core';
+import {Card, CardHeader, CardContent} from '@material-ui/core';
+import {Row, Col} from 'react-bootstrap';
+import {Paper, withStyles, Grid} from '@material-ui/core';
+
+const styles = theme => ({
+  margin: {
+    margin: theme.spacing.unit * 1,
+  },
+  padding: {
+    padding: theme.spacing.unit,
+  },
+});
+
+class Profile extends Component {
+  continue = e => {
+    e.preventDefault ();
+    this.props.nextStep ();
+  };
+
+  render () {
+    const {values} = this.props;
+    const {classes} = this.props;
+    return (
+      <Paper className={classes.padding}>
+        <Card>
+          <CardHeader title="Personal Details" />
+        </Card>
+        <CardContent>
+          <div className={classes.margin}>
+            <Grid container spacing={2} alignItems="center" lg={12}>
+              <Grid item md={6} sm={12} xs={12} lg={6}>
+                <TextField
+                  margin="dense"
+                  variant="outlined"
+                  name="name"
+                  label="Name"
+                  style={{width: '80%'}}
+                  required
+                  value={values.name}
+                  onChange={this.props.handleChange}
+                />
+              </Grid>
+              <Grid item md={6} sm={12} xs={12} lg={6}>
+                <TextField
+                  margin="dense"
+                  label="Email"
+                  variant="outlined"
+                  name="email"
+                  required
+                  style={{alignItems: 'left', width: '80%'}}
+                  value={values.email}
+                  onChange={this.props.handleChange}
+                />
+              </Grid>
+
+              <Grid item lg={6} xs={12} sm={12} md={6}>
+                <TextField
+                  margin="dense"
+                  label="Phone Number"
+                  variant="outlined"
+                  name="phone"
+                  style={{alignItems: 'left', width: '80%'}}
+                  value={values.phone}
+                  onChange={this.props.handleChange}
+                />
+              </Grid>
+            <Container className={classes.margin}>
+              <Row>
+                <Col lg={3} xs={0} />
+                <Col lg={3} xs={5}>
+                  <Button
+                    variant="contained"
+                    color="secondary"
+                    onClick={this.nextStep}
+                    disabled
+                  >
+                    Back
+                  </Button>
+                </Col>
+                <Col lg={3} xs={5}>
+                  <Button
+                    variant="contained"
+                    color="secondary"
+                    onClick={this.continue}
+                  >
+                    Next
+                  </Button>
+                </Col>
+                <Col lg={3} xs={1} />
+              </Row>
+            </Container>
+            </Grid>
+            {/* <Button
+              variant="contained"
+              color="secondary"
+              onClick={this.createAndDownloadPDF}
+              endIcon={<GetAppIcon />}
+            >
+              Generate PDF
+            </Button> */}
+          </div>
+        </CardContent>
+        <p className="text-center text-muted">Page 1 </p>
+      </Paper>
+    );
+  }
+}
+
+export default withStyles (styles) (Profile);
